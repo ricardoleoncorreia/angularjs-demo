b67298438557bc34e07309ea05ab2f4eb66222e1
Ricardo Leon <ricardoleoncorreia@gmail.com>
2018-11-28 10:31:23 -0300

    Create a hero service

--- app/components/heroes/heroes.component.js
+++ app/components/heroes/heroes.component.js
@@ -3,24 +3,8 @@ app.component('appHeroes', {
   controller: heroesController
 });
 
-function heroesController($scope){
-  // $scope.selectedHero = {}
-  $scope.hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  $scope.HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+function heroesController($scope, HeroService){
+  $scope.HEROES = HeroService.getHeroes();
   $scope.onSelect = function(hero){
     $scope.selectedHero = hero;
   }
